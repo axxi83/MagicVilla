cb87e5d59a1d1846daa6195bfe2fdfb9ea91dbff
Luis Aravena <laravena21@gmail.com>
2023-06-02 11:01:21 -0400

    test

---
+++ MagicVilla_API/Modelos/Dto/VillaDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_API.Modelos.Dto
+{
+    public class VillaDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Nombre { get; set; }
+
+        public int Ocupantes { get; set; }
+        public int MetrosCuadrados { get; set; }
+
+    }
+}
